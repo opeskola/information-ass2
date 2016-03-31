d007ab263ea6ea20769892cf95dfd0ec4bb555fd
Wille Keltikangas <wille.keltikangas@gofore.com>
2016-03-31 14:49:10 +0300

    Added queryparser for words

--- src/main/java/LuceneSearchApp.java
+++ src/main/java/LuceneSearchApp.java
@@ -17,12 +17,13 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.queryparser.classic.QueryParser;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -47,111 +48,44 @@ public class LuceneSearchApp {
 		IndexWriter w = new IndexWriter(luceneIndex, config);
 
 		for (DocumentInCollection doc : docs) {
-			Document luceneDocument = new Document();
-			luceneDocument.add(new TextField("title", doc.getTitle(), Field.Store.YES));
-			luceneDocument.add(new TextField("abstract", doc.getAbstractText(), Field.Store.YES));
-			//luceneDocument.add(new LongField("publish_date", doc.getPubDate().getTime(), Field.Store.YES));
-			w.addDocument(luceneDocument);
+			if (doc.getSearchTaskNumber() == 1) {
+				Document luceneDocument = new Document();
+				//luceneDocument.add(new TextField("title", doc.getTitle(), Field.Store.YES));
+				luceneDocument.add(new TextField("abstract", doc.getAbstractText(), Field.Store.YES));
+				luceneDocument.add(new IntField("relevance", doc.isRelevant() ? 1 : 0, Field.Store.YES));
+				w.addDocument(luceneDocument);
+			}
 		}
 		w.close();
 	}
 	
-	public List<String> search(List<String> inTitle, List<String> notInTitle, List<String> inDescription, List<String> notInDescription, String startDate, String endDate) throws IOException {
+	public List<String> search(String words, Analyzer analyzer) throws IOException, ParseException {
 		
-		printQuery(inTitle, notInTitle, inDescription, notInDescription, startDate, endDate);
+		System.out.println("query: " + words);
 		List<String> results = new LinkedList<String>();
 
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		QueryParser parser = new QueryParser("abstract", analyzer);
+		Query query = parser.parse(words);
+
+		/*
 		BooleanQuery.Builder queryBuilder = new BooleanQuery.Builder();
-
-		if (inTitle != null) {
-			BooleanQuery titleQuery = createBooleanQuery("title", inTitle, BooleanClause.Occur.MUST);
-			queryBuilder.add(titleQuery, BooleanClause.Occur.MUST);
+		for (String word: words) {
+			queryBuilder.add(new TermQuery(new Term("abstract", word)), BooleanClause.Occur.SHOULD);
 		}
-
-		if (notInTitle != null) {
-			BooleanQuery notTitleQuery = createBooleanQuery("title", notInTitle, BooleanClause.Occur.SHOULD);
-			queryBuilder.add(notTitleQuery, BooleanClause.Occur.MUST_NOT);
-		}
-
-		if (inDescription != null) {
-			BooleanQuery descriptionQuery = createBooleanQuery("description", inDescription, BooleanClause.Occur.MUST);
-			queryBuilder.add(descriptionQuery, BooleanClause.Occur.MUST);
-		}
-
-		if (notInDescription != null) {
-			BooleanQuery notDescriptionQuery = createBooleanQuery("description", notInDescription, BooleanClause.Occur.SHOULD);
-			queryBuilder.add(notDescriptionQuery, BooleanClause.Occur.MUST_NOT);
-		}
-
-		Long parsedStartDate = null;
-		Long parsedEndDate = null;
-
-		if (startDate != null) {
-			try {
-				Date start = getStartOfDay(format.parse(startDate));
-				parsedStartDate = start.getTime();
-			} catch (Exception e) {
-				// Ignore
-			}
-		}
-
-		if (endDate != null) {
-			try {
-				Date end = getEndOfDay(format.parse(endDate));
-				parsedEndDate = end.getTime();
-			} catch (Exception e) {
-				// Ignore
-			}
-		}
-
-		NumericRangeQuery dateQuery = NumericRangeQuery.newLongRange("publish_date", parsedStartDate, parsedEndDate, true, true);
-		queryBuilder.add(dateQuery, BooleanClause.Occur.MUST);
+		BooleanQuery query = queryBuilder.build();
+		*/
 
 		IndexReader reader = DirectoryReader.open(luceneIndex);
 		IndexSearcher searcher = new IndexSearcher(reader);
-		TopDocs docs = searcher.search(queryBuilder.build(), reader.numDocs());
+		TopDocs docs = searcher.search(query, reader.numDocs());
 
 		for (ScoreDoc hit: docs.scoreDocs) {
 			Document doc = searcher.doc(hit.doc);
-			IndexableField title = doc.getField("title");
-			results.add(title.stringValue());
+			IndexableField abstr = doc.getField("abstract");
+			results.add(abstr.stringValue());
 		}
 
 		return results;
-	}
-
-	private Date getEndOfDay(Date date) {
-		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-		calendar.setTime(date);
-		calendar.add(Calendar.DAY_OF_MONTH, 1);
-		calendar.set(Calendar.HOUR_OF_DAY, 23);
-		calendar.set(Calendar.MINUTE, 59);
-		calendar.set(Calendar.SECOND, 59);
-		calendar.set(Calendar.MILLISECOND, 999);
-		return calendar.getTime();
-	}
-
-	private Date getStartOfDay(Date date) {
-		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-		calendar.setTime(date);
-		calendar.add(Calendar.DAY_OF_MONTH, 1);
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MILLISECOND, 0);
-		return calendar.getTime();
-	}
-
-	private Date getStartOfNextDay(Date date) {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(date);
-		calendar.add(Calendar.DAY_OF_MONTH, 1);
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MILLISECOND, 0);
-		return calendar.getTime();
 	}
 
 	private BooleanQuery createBooleanQuery(String fieldName, List<String> tokens, BooleanClause.Occur clause) {
@@ -160,38 +94,6 @@ public class LuceneSearchApp {
 			queryBuilder.add(new TermQuery(new Term(fieldName, token)), clause);
 		}
 		return queryBuilder.build();
-	}
-
-	public void printQuery(List<String> inTitle, List<String> notInTitle, List<String> inDescription, List<String> notInDescription, String startDate, String endDate) {
-		System.out.print("Search (");
-		if (inTitle != null) {
-			System.out.print("in title: "+inTitle);
-			if (notInTitle != null || inDescription != null || notInDescription != null || startDate != null || endDate != null)
-				System.out.print("; ");
-		}
-		if (notInTitle != null) {
-			System.out.print("not in title: "+notInTitle);
-			if (inDescription != null || notInDescription != null || startDate != null || endDate != null)
-				System.out.print("; ");
-		}
-		if (inDescription != null) {
-			System.out.print("in description: "+inDescription);
-			if (notInDescription != null || startDate != null || endDate != null)
-				System.out.print("; ");
-		}
-		if (notInDescription != null) {
-			System.out.print("not in description: "+notInDescription);
-			if (startDate != null || endDate != null)
-				System.out.print("; ");
-		}
-		if (startDate != null) {
-			System.out.print("startDate: "+startDate);
-			if (endDate != null)
-				System.out.print("; ");
-		}
-		if (endDate != null)
-			System.out.print("endDate: "+endDate);
-		System.out.println("):");
 	}
 	
 	public void printResults(List<String> results) {
@@ -204,7 +106,7 @@ public class LuceneSearchApp {
 			System.out.println(" no results");
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
 		if (args.length > 0) {
 			LuceneSearchApp engine = new LuceneSearchApp();
 			
@@ -213,12 +115,12 @@ public class LuceneSearchApp {
 			List<DocumentInCollection> docs = parser.getDocuments();
 
 			List<String> queries = Arrays.asList("motion tracking", "gesture user interface", "motion detection user interface");
-			Analyzer analyzer;
+			Analyzer analyzer = null;
 			switch(Integer.valueOf(args[1])) {
 				case 1:
 					// VSM (lucene default) with Porter stemmer and stop words
-					//analyzer = new PorterAnalyzer();
-					analyzer = new EnglishAnalyzer();
+					analyzer = new PorterAnalyzer();
+					//analyzer = new EnglishAnalyzer();
 					engine.index(docs, analyzer);
 					break;
 				case 2:
@@ -253,9 +155,14 @@ public class LuceneSearchApp {
 					break;
 				default:
 					System.out.println("Indexing option was missing");
+					break;
 			}
-
-
+			Random random = new Random();
+			//List<String> queryWords = Arrays.asList(queries.get(random.nextInt(3)).split(" "));
+			List<String> queryWords = Arrays.asList("gesture", "user", "interface");
+			//List<String> queryWords = Arrays.asList("human", "interface");
+			List<String> results = engine.search("gesture AND user AND interface", analyzer);
+			engine.printResults(results);
 		}
 		else
 			System.out.println("ERROR: the path of a Document file has to be passed as a command line argument.");
--- src/main/java/PorterAnalyzer.java
+++ src/main/java/PorterAnalyzer.java
@@ -2,12 +2,12 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.LowerCaseTokenizer;
 import org.apache.lucene.analysis.en.PorterStemFilter;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 class PorterAnalyzer extends Analyzer {
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
-        Tokenizer source = new LowerCaseTokenizer();
+        Tokenizer source = new StandardTokenizer(); //new LowerCaseTokenizer();
         return new TokenStreamComponents(source, new PorterStemFilter(source));
     }
 }
